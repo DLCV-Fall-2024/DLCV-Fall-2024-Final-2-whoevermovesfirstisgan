111f253698e6b67dd3a169b964b8881f6efd1ace
tiwang <tiwang@media.ee.ntu.edu.tw>
2024-12-26 21:27:32 +0800

    fix sample denpendency

---
+++ code/download.sh
@@ -0,0 +1,5 @@
+gdown '1azq_YMz5l_jsff9nI-lA8Ez00Iq_aE-3' -O './experiments/pretrained_models/chilloutmix.zip'
+unzip './experiments/pretrained_models/chilloutmix.zip' -d './experiments/pretrained_models/'
+
+gdwon '15SLZAAyvSvVvT2cNTRzK3SYyQjACCsfh' -O './fusion/graycat+wearableglasses.tar.gz'
+tar -xcvf './fusion/graycat+wearableglasses.tar.gz' -C './fusion/'
--- code/go_sample.sh
+++ code/go_sample.sh
@@ -1,15 +1,15 @@
-# echo "[setting] ==================== prompt0 ===================="
-# python3 sample.py \
-#     --config_file ./configs_ours/prompt0.yaml
+echo "[setting] ==================== prompt0 ===================="
+python3 sample.py \
+    --config_file ./configs_ours/prompt0.yaml
 
-# echo "[setting] ==================== prompt1 ===================="
-# python3 sample.py \
-#     --config_file ./configs_ours/prompt1.yaml
+echo "[setting] ==================== prompt1 ===================="
+python3 sample.py \
+    --config_file ./configs_ours/prompt1.yaml
 
-# echo "[setting] ==================== prompt2 ===================="
-# python3 sample.py \
-#     --config_file ./configs_ours/prompt2.yaml
+echo "[setting] ==================== prompt2 ===================="
+python3 sample.py \
+    --config_file ./configs_ours/prompt2.yaml
 
-# echo "[setting] ==================== prompt3 ===================="
-# python3 test_edlora.py \
-#     -opt ./configs_ours/prompt3.yaml
+echo "[setting] ==================== prompt3 ===================="
+python3 test_edlora.py \
+    -opt ./configs_ours/prompt3.yaml
