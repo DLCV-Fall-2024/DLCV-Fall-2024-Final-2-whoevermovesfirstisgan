9ec591e74b7f81d55f0cfd00e806a283e17e6463
Yulun990404 <153384971+ylchiangsogood@users.noreply.github.com>
2024-12-26 22:43:58 +0800

    Update download.sh

--- code/download.sh
+++ code/download.sh
@@ -2,4 +2,4 @@ gdown '1azq_YMz5l_jsff9nI-lA8Ez00Iq_aE-3' -O './experiments/pretrained_models/ch
 unzip './experiments/pretrained_models/chilloutmix.zip' -d './experiments/pretrained_models/'
 
 gdown '15SLZAAyvSvVvT2cNTRzK3SYyQjACCsfh' -O './fusion/graycat+wearableglasses.tar.gz'
-tar -xcvf './fusion/graycat+wearableglasses.tar.gz' -C './fusion/'
+tar -xzvf './fusion/graycat+wearableglasses.tar.gz' -C './fusion/'
