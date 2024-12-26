58e4a985a384c162f5ba1bb026b2d0d81b95bd92
tiwang <tiwang@media.ee.ntu.edu.tw>
2024-12-26 20:24:48 +0800

    dev upload sampling code

---
+++ code/go_testEDLoRA.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=0 python3 test_edlora.py \
+    -opt ./configs_ours/prompt3.yml
---
+++ code/go_trainEDLoRA.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=0 python3 train_edlora.py \
+    -opt ./configs/edlora/train/gray_cat_4_p0.yml
